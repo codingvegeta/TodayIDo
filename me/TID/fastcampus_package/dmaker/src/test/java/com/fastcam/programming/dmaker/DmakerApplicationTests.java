3defdd900908be88e355d365e4709ed8df3d575d
codingvegeta <hong090992@gmail.com>
2022-12-23 20:37:08 +0900

    2022/12/23 "패스트 캠퍼스 한번에 끝내는 완전판 초격차 패키지 를 위한 파일 생성"

---
+++ me/TID/fastcampus_package/dmaker/src/test/java/com/fastcam/programming/dmaker/DmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcam.programming.dmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
