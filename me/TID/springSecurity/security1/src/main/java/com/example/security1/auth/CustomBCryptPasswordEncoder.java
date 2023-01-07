f627597a3c71f672b1a2d02d6e30eac8e732e7ac
codingvegeta <hong090992@gmail.com>
2023-01-08 02:07:44 +0900

    2023/01/08 "spring Security 공부중"

---
+++ me/TID/springSecurity/security1/src/main/java/com/example/security1/auth/CustomBCryptPasswordEncoder.java
@@ -0,0 +1,8 @@
+package com.example.security1.auth;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBCryptPasswordEncoder extends BCryptPasswordEncoder {
+}
