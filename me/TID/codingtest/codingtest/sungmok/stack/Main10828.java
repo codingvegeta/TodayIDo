36492cb9184dd66b260237aedce2ce3c9ffa7e28
codingvegeta <hong090992@gmail.com>
2023-01-13 03:08:28 +0900

    2022/01/13 "알고리즘 문제 풀이"

--- me/TID/codingtest/codingtest/sungmok/stack/Main10828.java
+++ me/TID/codingtest/codingtest/sungmok/stack/Main10828.java
@@ -1,67 +1,45 @@
-//package sungmok.stack;
-//
-//import org.w3c.dom.css.CSSImportRule;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Scanner;
-//
-///**
-// * push X: 정수 X를 스택에 넣는 연산이다.
-// * pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-// * size: 스택에 들어있는 정수의 개수를 출력한다.
-// * empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
-// * top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-// */
-//public class Main10828 {
-//
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        Stack sungmok.stack = new Stack(sc.nextInt());
-//        sungmok.stack.sc.next(10);
-//        System.out.println(sungmok.stack.empty());
-//        System.out.println("sungmok.stack.top() = " + sungmok.stack.top());
-//
-//    }
-//}
-//
-//class Stack{
-//    public Stack(int size) {
-//        this.size = size;
-//    }
-//
-//    int size;
-//    int[] arraysStack = new int[size];
-//    int tailPosition = 0;
-//
-//
-//    public void push(int x) {
-//        stackList.add(x);
-//        ++tailPosition;
-//    }
-//
-//    public int pop() {
-//        if (stackList.get(tailPosition) > 0) {
-//            return stackList.get(tailPosition--);
-//        } else {
-//            return -1;
-//        }
-//    }
-//
-//    public int size() {
-//        return tailPosition;
-//    }
-//
-//    public int empty() {
-//        return tailPosition == 0 ? 1 : 0;
-//    }
-//
-//    public int top() {
-//        if (tailPosition > 0) {
-//            return stackList.get(tailPosition - 1);
-//        } else {
-//            return -1;
-//        }
-//    }
-//
-//}
+package sungmok.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main10828 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack();
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            if (str.equals("push")) {
+                stack.push(Integer.parseInt(st.nextToken()));
+            } else if (str.equals("top")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.peek());
+                }
+            } else if (str.equals("size")) {
+                System.out.println(stack.size());
+            } else if (str.equals("empty")) {
+                if (stack.isEmpty()) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+            } else if (str.equals("pop")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                System.out.println(stack.pop());
+                }
+            }
+        }
+    }
+}
+
+
