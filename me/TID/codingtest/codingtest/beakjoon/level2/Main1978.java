c797baa56cc0d2d4c463e2931a1e091c79b199ac
codingvegeta <hong090992@gmail.com>
2023-01-17 01:39:51 +0900

    2022/1/16 "알고리즘 공부하기" 맞춰버렸다.

--- me/TID/codingtest/codingtest/beakjoon/level2/Main1978.java
+++ me/TID/codingtest/codingtest/beakjoon/level2/Main1978.java
@@ -3,8 +3,7 @@ package beakjoon.level2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main1978 {
@@ -12,38 +11,22 @@ public class Main1978 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> prime = new ArrayList();
-        prime.add(2);
-        prime.add(3);
+        boolean[] prime = new boolean[1001];
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
         int count = 0;
-        for (int i = 5; i <= 1000; i += 2) {
-            boolean flag = false;
-            for (int j = 1; prime.get(j) * prime.get(j) <= i; j++) {
-                if (i % prime.get(j) == 0) {
-                    flag = true;
-                    break;
-                }
+        for (int i = 2; i <= Math.sqrt(1000); i++) {
+            int j = 2;
+            while (i * j <= 1000) {
+                prime[i * j] = false;
+                j++;
             }
-            if (!flag) {
-                prime.add(i);
-            }
+
         }
-        System.out.println(prime);
+
         for (int i = 0; i < n; i++) {
-            boolean flag = false;
             int number = Integer.parseInt(st.nextToken());
-            if (number == 2 || number == 3) {
-                count++;
-            }
-            for (int j = 0; prime.get(j) < number; j++) {
-                if (number == 0 || number == 1) {
-                    flag = true;
-                }
-                if (number % prime.get(j) == 0) {
-                    flag = true;
-                }
-            }
-            if (!flag) {
+            if (prime[number] == true) {
                 count++;
             }
         }
