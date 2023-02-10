e82b2fce16487ad842708d35c8b4751dbac306e2
codingvegeta <hong090992@gmail.com>
2023-02-11 01:46:57 +0900

    2022/2/11 "알고리즘 공부하기"

---
+++ me/TID/codingtest/codingtest/beakjoon/gold3/Main10986.java
@@ -0,0 +1,28 @@
+//package beakjoon.gold3;
+//import java.io.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.StringTokenizer;
+//
+//public class Main10986 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int n = Integer.parseInt(st.nextToken());
+//        int m = Integer.parseInt(st.nextToken());
+//
+//        long[] a = new long[n];
+//        st = new StringTokenizer(br.readLine());
+//        for (int i = 0; i < n; i++) {
+//            a[i] = Long.parseLong(st.nextToken());
+//        }
+//
+//        List<Integer> d = new ArrayList<>();
+//        for (int i = 0; i < n - 1; i++) {
+//            for (int j = i + 1; j < n; j++) {
+//
+//            }
+//        }
+//    }
+//}
