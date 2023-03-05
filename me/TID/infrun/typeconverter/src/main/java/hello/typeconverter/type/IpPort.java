651836ec2ca9d639927db739364fcc2eb4351aa1
codingvegeta <hong090992@gmail.com>
2023-03-05 22:28:28 +0900

    2023/3/5 "스프링 mvc2"

---
+++ me/TID/infrun/typeconverter/src/main/java/hello/typeconverter/type/IpPort.java
@@ -0,0 +1,18 @@
+package hello.typeconverter.type;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@EqualsAndHashCode
+public class IpPort {
+
+    private String ip;
+    private int port;
+
+    public IpPort(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+}
