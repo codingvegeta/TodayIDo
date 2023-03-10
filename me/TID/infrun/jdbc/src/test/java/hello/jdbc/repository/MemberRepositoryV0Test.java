dfc34ea3368cea00115124425b8e64035444dc85
codingvegeta <hong090992@gmail.com>
2023-03-10 13:59:39 +0900

    2023/3/10 "스프링 db 1"

---
+++ me/TID/infrun/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,28 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV2", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
