d6b5809409ffb7e3db23b374194575e7e692e39a
codingvegeta <hong090992@gmail.com>
2023-03-14 00:16:40 +0900

    2023/3/14 "스프링 db 1"

---
+++ me/TID/infrun/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,44 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ * 클래스에 붙이면 호출 가능한 public 메서드가 AOP 적용 대상이 된다.
+ */
+@Slf4j
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
