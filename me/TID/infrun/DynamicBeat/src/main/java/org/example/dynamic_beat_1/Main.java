df2276fbbb6a36b1b65abef4abdca6d861601941
codingvegeta <hong090992@gmail.com>
2023-03-17 00:28:09 +0900

    2023/3/17 "자바 swing"

--- me/TID/infrun/DynamicBeat/src/main/java/org/example/dynamic_beat_1/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example.dynamic_beat_1;
-
-public class Main {
-    public static final int SCREEN_WIDTH = 1280;
-    public static final int SCREEN_HEIGHT = 720;
-    public static void main(String[] args) {
-        new DynamicBeat();
-    }
-}
