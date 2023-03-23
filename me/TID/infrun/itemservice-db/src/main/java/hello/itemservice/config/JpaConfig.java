4876ca22f252b5ff3186bc12a86191cbc6534409
codingvegeta <hong090992@gmail.com>
2023-03-23 14:48:26 +0900

    2023/3/23 "스프링 db 2"

---
+++ me/TID/infrun/itemservice-db/src/main/java/hello/itemservice/config/JpaConfig.java
@@ -0,0 +1,30 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class JpaConfig {
+
+    private final EntityManager em;
+
+    public JpaConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JpaItemRepository(em);
+    }
+}
--- me/TID/infrun/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ me/TID/infrun/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,17 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name",length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
