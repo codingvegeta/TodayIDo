ba26f234d885279b82a3641fbdeaa061a20e44fb
codingvegeta <hong090992@gmail.com>
2023-03-24 14:11:32 +0900

    2023/3/24 "스프링 db 2"

---
+++ me/TID/infrun/itemservice-db/src/main/java/hello/itemservice/config/SpringDataJpaConfig.java
@@ -0,0 +1,31 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV2;
+import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class SpringDataJpaConfig {
+
+    private final SpringDataJpaItemRepository springDataJpaItemRepository;
+
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JpaItemRepositoryV2(springDataJpaItemRepository);
+    }
+}
--- me/TID/infrun/itemservice-db/src/main/java/hello/itemservice/service/ItemServiceV1.java
+++ me/TID/infrun/itemservice-db/src/main/java/hello/itemservice/service/ItemServiceV1.java
@@ -4,6 +4,7 @@ import hello.itemservice.domain.Item;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
+import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
