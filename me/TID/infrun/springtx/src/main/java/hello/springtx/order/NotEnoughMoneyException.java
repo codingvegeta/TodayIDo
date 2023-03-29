a8a1a40140ccde74bf3108c73314e549892deb68
codingvegeta <hong090992@gmail.com>
2023-03-29 13:24:22 +0900

    2023/3/29 "스프링 db 2"

---
+++ me/TID/infrun/springtx/src/main/java/hello/springtx/order/NotEnoughMoneyException.java
@@ -0,0 +1,7 @@
+package hello.springtx.order;
+
+public class NotEnoughMoneyException extends Exception{
+    public NotEnoughMoneyException(String message) {
+        super(message);
+    }
+}
---
+++ me/TID/infrun/springtx/src/main/java/hello/springtx/order/OrderRepository.java
@@ -0,0 +1,7 @@
+package hello.springtx.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
