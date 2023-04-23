dd914d5e82cada05fab53d1cdacf54a22991e50a
codingvegeta <hong090992@gmail.com>
2023-04-24 00:19:42 +0900

    2023/4/24 "스프링 핵심원리 고급"

---
+++ me/TID/infrun/aop/src/test/java/hello/aop/proxyvs/code/ProxyDITest.java
@@ -0,0 +1,29 @@
+package hello.aop.proxyvs.code;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) //JDK 동적 프록시
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) //JDK 동적 프록시
+@Import(ProxyDIAspect.class)
+public class ProxyDITest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberServiceImpl memberServiceImpl;
+
+    @Test
+    void go() {
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
+    }
+}
