81428685211718042151bf5368d3a415acd88666
codingvegeta <hong090992@gmail.com>
2023-05-30 00:37:03 +0900

    23/5/30 javascript

---
+++ me/TID/javascript/modernjs/9/09-19.js
@@ -0,0 +1,23 @@
+// console.log('Cat' || 'Dog');
+// console.log(false || 'Dog');
+// console.log('Cat' || false);
+
+// console.log('Cat' && 'Dog');
+// console.log(false && 'Dog');
+// console.log('Cat' && false);
+
+let annimal0 = 'Cat' || 'Dog';
+let animal1 = false || 'Dog';
+let animal2 = 'Cat' || false;
+
+console.log(annimal0);
+console.log(animal1);
+console.log(animal2);
+
+let animal3 = 'Cat' && 'Dog';
+let animal4 = false && 'Dog';
+let animal5 = 'Cat' && false;
+
+console.log(animal3);
+console.log(animal4);
+console.log(animal5);
