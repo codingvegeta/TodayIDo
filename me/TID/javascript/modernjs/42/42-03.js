45d105ef00dc09c71bc91384efdf77ce5e5a967e
codingvegeta <hong090992@gmail.com>
2023-06-06 01:57:55 +0900

    23/6/6 javascript

---
+++ me/TID/javascript/modernjs/42/42-03.js
@@ -0,0 +1,20 @@
+function sleep(func, delay){
+
+    const delayUntil = Date.now() + delay;
+
+    while (Date.now() < delayUntil);
+
+    func();
+}
+
+function foo() {
+    console.log('foo');
+}
+
+function bar() {
+    console.log('bar');
+}
+
+setTimeout(foo, 3 * 1000);
+
+bar();
