7eff3ca6c8a59eb95be385c413039062598496fc
codingvegeta <hong090992@gmail.com>
2023-06-26 01:03:14 +0900

    2023/6/26 "modernjava"

---
+++ me/TID/modernjava/src/chap11/CarV1.java
@@ -0,0 +1,11 @@
+package chap11;
+
+public class CarV1 {
+
+  private Insurance insurance;
+
+  public Insurance getInsurance() {
+    return insurance;
+  }
+
+}
---
+++ me/TID/modernjava/src/chap11/PersonV1.java
@@ -0,0 +1,11 @@
+package chap11;
+
+public class PersonV1 {
+
+  private CarV1 car;
+
+  public CarV1 getCar() {
+    return car;
+  }
+
+}
