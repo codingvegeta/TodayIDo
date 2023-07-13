31830b85bfce22eb52464fea59e7a7bcb84a5877
codingvegeta <hong090992@gmail.com>
2023-07-14 03:34:54 +0900

    2023/7/14 "스프링 시큐리티"

---
+++ me/TID/infrun/core-spring-security/src/main/java/com/example/corespringsecurity/domain/entity/Account.java
@@ -0,0 +1,38 @@
+package com.example.corespringsecurity.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@ToString(exclude = {"userRoles"})
+@Builder
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+    @Column
+    private int age;
+
+    @Column
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade={CascadeType.ALL})
+    @JoinTable(name = "account_roles", joinColumns = { @JoinColumn(name = "account_id") }, inverseJoinColumns = {
+            @JoinColumn(name = "role_id") })
+    private Set<Role> userRoles = new HashSet<>();
+}
---
+++ me/TID/infrun/core-spring-security/src/main/java/com/example/corespringsecurity/domain/entity/Role.java
@@ -0,0 +1,38 @@
+package com.example.corespringsecurity.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "ROLE")
+@Getter
+@Setter
+@ToString(exclude = {"users", "resourcesSet"})
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Role {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "role_id")
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @Column(name = "role_desc")
+    private String roleDesc;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "roleSet")
+    @OrderBy("ordernum desc")
+    private Set<Resources> resourcesSet = new LinkedHashSet<>();
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "userRoles")
+    private Set<Account> accounts = new HashSet<>();
+}
