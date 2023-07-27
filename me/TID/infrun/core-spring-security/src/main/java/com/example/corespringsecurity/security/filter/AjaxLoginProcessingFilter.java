0b303bfdff8333bc6ecd46a243f3ce99a2fae057
codingvegeta <hong090992@gmail.com>
2023-07-27 12:59:59 +0900

    2023/7/27 "스프링 시큐리티"

--- me/TID/infrun/core-spring-security/src/main/java/com/example/corespringsecurity/security/filter/AjaxLoginProcessingFilter.java
+++ me/TID/infrun/core-spring-security/src/main/java/com/example/corespringsecurity/security/filter/AjaxLoginProcessingFilter.java
@@ -1,13 +1,12 @@
 package com.example.corespringsecurity.security.filter;
 
-import com.example.corespringsecurity.domain.AccountDto;
+import com.example.corespringsecurity.domain.dto.AccountDto;
 import com.example.corespringsecurity.security.token.AjaxAuthenticationToken;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
