0b4bf5f330df0f916fda649d8d79c266282671af
codingvegeta <hong090992@gmail.com>
2023-08-14 10:24:38 +0900

    2023/8/14 "알고리즘 문제풀이"

---
+++ me/TID/codingtest/codingtest/beakjoon/dd/Main14681.java
@@ -0,0 +1,20 @@
+package beakjoon.dd;
+
+import java.util.Scanner;
+
+public class Main14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        } else if (x < 0 && y < 0) {
+            System.out.println(3);
+        } else if (x < 0 && y > 0) {
+            System.out.println(2);
+        } else {
+            System.out.println(4);
+        }
+    }
+}
