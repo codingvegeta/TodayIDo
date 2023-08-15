e7f25af89a1788c2eb10591f7cc4f0817ffd4722
codingvegeta <hong090992@gmail.com>
2023-08-16 00:19:55 +0900

    2023/8/16 "알고리즘 문제풀이"

---
+++ me/TID/codingtest/codingtest/beakjoon/dd/Main8393.java
@@ -0,0 +1,15 @@
+package beakjoon.dd;
+
+import java.util.Scanner;
+
+public class Main8393 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
