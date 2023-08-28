022a1c47ee2adf324b8e1e48ac441c199fcf8afa
codingvegeta <hong090992@gmail.com>
2023-08-29 00:35:56 +0900

    2023/8/29 "알고리즘 문제풀이"

---
+++ me/TID/codingtest/codingtest/beakjoon/five/Main11718.java
@@ -0,0 +1,12 @@
+package beakjoon.five;
+
+import java.util.Scanner;
+
+public class Main11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
