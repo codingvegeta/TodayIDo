921faf6c3de66c39b83649b60eef57bd44cbe517
codingvegeta <hong090992@gmail.com>
2023-09-10 09:14:35 +0900

    2023/09/10 "알고리즘 문제풀이"

---
+++ me/TID/codingtest/codingtest/beakjoon/six/Main25083.java
@@ -0,0 +1,12 @@
+package beakjoon.six;
+
+public class Main25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
