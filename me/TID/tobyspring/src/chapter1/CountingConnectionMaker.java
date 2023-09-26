998efbcaa31d0cb1a2d5fe8360db9ae1132a2602
codingvegeta <hong090992@gmail.com>
2023-09-27 00:43:45 +0900

    2023/09/27 "토비의 스프링"

---
+++ me/TID/tobyspring/src/chapter1/CountingConnectionMaker.java
@@ -0,0 +1,27 @@
+package chapter1;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CountingConnectionMaker implements ConnectionMaker
+{
+    int counter = 0;
+    private ConnectionMaker realConnectionMaker;
+
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker)
+    {
+        this.realConnectionMaker = realConnectionMaker;
+    }
+
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException
+    {
+        this.counter++;
+        return realConnectionMaker.makeConnection();
+    }
+
+    public int getCounter()
+    {
+        return this.counter;
+    }
+}
