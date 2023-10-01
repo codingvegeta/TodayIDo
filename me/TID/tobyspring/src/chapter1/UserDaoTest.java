137ffc8c7366437a1c35b53f1a71c7dcd0c9e3de
codingvegeta <hong090992@gmail.com>
2023-10-01 13:00:00 +0000

    2023/10/3 "토비의 스프링"

---
+++ me/TID/tobyspring/src/chapter1/UserDaoTest.java
@@ -0,0 +1,44 @@
+package chapter1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.sql.SQLException;
+
+public class UserDaoTest
+{
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, IllegalAccessException
+    {
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext("chapter1/factorybean.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+//
+//        dao.add(user);
+//
+//        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        if (!user.getName().equals(user2.getName()))
+        {
+            System.out.println("테스트 실패 (name)");
+        }
+        else if (!user.getPassword().equals(user2.getPassword()))
+        {
+            System.out.println("테스트 실패 (password)");
+        }
+        else
+        {
+            System.out.println("조회 테스트 성공");
+        }
+
+    }
+
+
+
+}
