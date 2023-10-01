8d3a12e6e254b740808cff9dcb8a12f0f67b8bc1
codingvegeta <hong090992@gmail.com>
2023-10-02 00:45:17 +0900

    2023/10/2 "토비의 스프링"

--- me/TID/tobyspring/src/chapter1/User.java
+++ me/TID/tobyspring/src/chapter1/User.java
@@ -1,10 +1,45 @@
 package chapter1;
 
+import chapter5.Level;
+
 public class User
 {
     String id;
     String name;
     String password;
+    Level level;
+    int login;
+    int recommend;
+
+    public Level getLevel()
+    {
+        return level;
+    }
+
+    public void setLevel(Level level)
+    {
+        this.level = level;
+    }
+
+    public int getLogin()
+    {
+        return login;
+    }
+
+    public void setLogin(int login)
+    {
+        this.login = login;
+    }
+
+    public int getRecommend()
+    {
+        return recommend;
+    }
+
+    public void setRecommend(int recommend)
+    {
+        this.recommend = recommend;
+    }
 
     public String getId()
     {
@@ -36,16 +71,30 @@ public class User
         this.password = password;
     }
 
+    public void upgradeLevel()
+    {
+        Level nextLevel = this.level.nextLevel();
+        if (nextLevel == null)
+        {
+            throw new IllegalStateException(this.level + "은 업그레이드가 불가능합니다.");
+        }else
+        {
+            this.level = nextLevel;
+        }
+    }
+
     public User()
     {
 
     }
 
-    public User(String id, String name, String password)
+    public User(String id, String name, String password, Level level, int login, int recommend)
     {
         this.id = id;
         this.name = name;
         this.password = password;
+        this.level = level;
+        this.login = login;
+        this.recommend = recommend;
     }
-
 }
