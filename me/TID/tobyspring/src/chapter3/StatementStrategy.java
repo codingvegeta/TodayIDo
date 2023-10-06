7e390148b036e8ee1c2351dca4c09d8664f70467
codingvegeta <hong090992@gmail.com>
2023-10-07 00:21:54 +0900

    2023/10/7 "토비의 스프링"

---
+++ me/TID/tobyspring/src/chapter3/StatementStrategy.java
@@ -0,0 +1,10 @@
+package chapter3;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface StatementStrategy
+{
+    PreparedStatement makePreparedStatement(Connection c) throws SQLException;
+}
