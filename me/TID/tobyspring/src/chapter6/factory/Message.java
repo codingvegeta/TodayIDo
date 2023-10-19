7d9539ecb856bcbca5eb9e0c3c40119bc3fec2fd
codingvegeta <hong090992@gmail.com>
2023-10-19 18:36:20 +0900

    2023/10/19 "토비의 스프링"

---
+++ me/TID/tobyspring/src/chapter6/factory/Message.java
@@ -0,0 +1,21 @@
+package chapter6.factory;
+
+public class Message
+{
+    String text;
+
+    private Message(String text)
+    {
+        this.text = text;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public static Message newMessage(String text)
+    {
+        return new Message(text);
+    }
+}
