c9c03320764dfda21f0f9ef8e6f8b746380134a8
codingvegeta <hong090992@gmail.com>
2023-10-25 10:28:21 +0900

    2023/10/25 "토비의 스프링"

---
+++ me/TID/tobyspring/src/chapter6/proxy/HelloTarget.java
@@ -0,0 +1,22 @@
+package chapter6.proxy;
+
+public class HelloTarget implements Hello
+{
+    @Override
+    public String sayHello(String name)
+    {
+        return "Hello " + name;
+    }
+
+    @Override
+    public String sayHi(String name)
+    {
+        return "Hi " + name;
+    }
+
+    @Override
+    public String sayThankYou(String name)
+    {
+        return "Thank You " + name;
+    }
+}
