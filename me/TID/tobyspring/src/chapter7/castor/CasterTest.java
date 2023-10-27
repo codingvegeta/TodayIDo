7f6c7fa86ef21a8960335d8734eef0d0cbd1ac48
codingvegeta <hong090992@gmail.com>
2023-10-27 10:55:08 +0900

    2023/10/27 "토비의 스프링"

---
+++ me/TID/tobyspring/src/chapter7/castor/CasterTest.java
@@ -0,0 +1,45 @@
+package chapter7.castor;
+
+import chapter7.jaxb.SqlType;
+import chapter7.jaxb.Sqlmap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.XmlMappingException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "CasterTest-context.xml")
+public class CasterTest
+{
+    @Autowired
+    Unmarshaller unmarshaller;
+
+    @Test
+    public void isNotNull()
+    {
+        assertNotNull(unmarshaller);
+    }
+    @Test
+    public void unmarshallSqlMap() throws XmlMappingException, IOException, JAXBException
+    {
+        Source xmlSource = new StreamSource(getClass().getResourceAsStream("sqlmap.xml"));
+
+        Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(xmlSource);
+
+        List<SqlType> sqlList = sqlmap.getSql();
+        assertThat(sqlList.size(), is(6));
+    }
+}
