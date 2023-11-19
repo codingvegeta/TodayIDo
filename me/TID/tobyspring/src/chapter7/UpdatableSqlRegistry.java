721484ea724cf1777ca8289993d03fb89db0ca5a
codingvegeta <hong090992@gmail.com>
2023-11-19 09:49:19 +0900

    2023/11/19

---
+++ me/TID/tobyspring/src/chapter7/UpdatableSqlRegistry.java
@@ -0,0 +1,10 @@
+package chapter7;
+
+import java.util.Map;
+
+public interface UpdatableSqlRegistry extends SqlRegistry
+{
+    public void updateSql(String key, String sql) throws SqlUpdateFailureException;
+
+    public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException;
+}
