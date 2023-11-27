637545f85377d3a1d8f988c190a39988da9f5819
codingvegeta <hong090992@gmail.com>
2023-11-27 11:44:37 +0900

    2023/11/27

---
+++ me/TID/tobyspring/src/vol2/chapter1/myproject/MyDao.java
@@ -0,0 +1,9 @@
+package vol2.chapter1.myproject;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyDao
+{
+}
