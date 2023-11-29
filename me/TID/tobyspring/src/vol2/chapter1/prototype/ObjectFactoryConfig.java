29a396119bab53bfa8826a316bc09713d906128c
codingvegeta <hong090992@gmail.com>
2023-11-30 00:18:40 +0900

    2023/11/30

---
+++ me/TID/tobyspring/src/vol2/chapter1/prototype/ObjectFactoryConfig.java
@@ -0,0 +1,17 @@
+package vol2.chapter1.prototype;
+
+import org.springframework.beans.factory.config.ObjectFactoryCreatingFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectFactoryConfig
+{
+    @Bean
+    public ObjectFactoryCreatingFactoryBean serviceRequestFactory()
+    {
+        ObjectFactoryCreatingFactoryBean factoryBean = new ObjectFactoryCreatingFactoryBean();
+        factoryBean.setTargetBeanName("serviceRequest");
+        return factoryBean;
+    }
+}
