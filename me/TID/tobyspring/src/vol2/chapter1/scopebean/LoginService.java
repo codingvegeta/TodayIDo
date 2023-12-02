e94a95235efc7e33f29f437cfd880c8621ad0fd2
codingvegeta <hong090992@gmail.com>
2023-12-03 00:07:15 +0900

    2023/12/3

---
+++ me/TID/tobyspring/src/vol2/chapter1/scopebean/LoginService.java
@@ -0,0 +1,16 @@
+package vol2.chapter1.scopebean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.inject.Provider;
+
+public class LoginService
+{
+//    @Autowired
+//    LoginUser loginUser;
+//
+//    public void login(Login login)
+//    {
+//        loginUser.setLoginID();
+//    }
+}
